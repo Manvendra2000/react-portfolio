12886b5038e6c39d268a525458140ce48e40a169
Manvendra2000 <mzealpro@gmail.com>
2024-06-20 03:12:16 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a highly motivated front-end developer with extensive experience as a freelancer. I have successfully created numerous front end websites utilising various web technologies, including WordPress and frameworks such as ReactJS. My expertise in crafting visually appealing and responsive user interfaces, combined with my strong problem-solving skills and attention to detail, has allowed me to deliver high-quality projects that meet client requirements and industry standards.  `;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile front end  developer with a passion for creating efficient and user-friendly frontend websites. With 1.5 years of professional experience, I have worked with a variety of technologies, including React, WordPress and Blockchain. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and learning about open-source contributions.`;
+
+export const EXPERIENCES = [
+  {
+    year: "March 2024 - July 2024",
+    role: "WordPress Developer",
+    company: "MJ Associate",
+    description: `Collaborated with stakeholders to design a website for an architectural studio using HTML, CSS, jQuery, and JavaScript. Developed a comprehensive list of the studio's services and managed project requirements and timelines effectively.`,
+    technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
+  },
+  {
+    year: "January 2024 - March 2024",
+    role: "WordPress Developer",
+    company: "CS Akshay Gupta",
+    description: `Developed a professional portfolio website for a company secretary using WordPress, incorporating plugins such as Elementor and WP Forms. The site effectively showcases all services offered, featuring a user-friendly design that highlights the secretary's skills and expertise.`,
+    technologies: ["PHP", "Elementor", "WordPress", "mySQL"],
+  },
+  {
+    year: "September 2023 - December 2023",
+    role: "WordPress Developer",
+    company: "Rajasthani Passenger",
+    description: `Developed a travel agency website using WordPress and Elementor. Integrated user-friendly contact forms with WPForms, streamlining the booking inquiry process. Handled all aspects of development, maintenance, and ongoing technical support.`,
+    technologies: ["WordPress", "WPForms", "Elementor", "Yoast SEO"],
+  },
+  {
+    year: "July 2023 - September 2023",
+    role: "WordPress Developer",
+    company: "My Health Page",
+    description: `Contributed to the development of website using WordPress and Updraft Plus. Created an online encyclopedia for the users with over hundreds of SEO optimized articles. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["WordPress", "Wordfence", "Updraft Plus", "PHP"],
+  },
+];
+
+export const PROJECTS = [
+ /* {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  }, */
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "WordPress", "PHP", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kota (324009), Rajasthan, India ",
+  phoneNo: "+91 9024762254",
+  email: "ms28.dev@gmail.com",
+};
